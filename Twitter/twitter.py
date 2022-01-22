1287ddd6bfdc31815addc6303cfa15463833cf38
Okimii <92546867+Okimii@users.noreply.github.com>
2022-01-21 23:11:56 -0400

    rewrite to follow pep8 max line

--- Twitter/twitter.py
+++ Twitter/twitter.py
@@ -2,10 +2,21 @@ import requests
 import json
 
 class Client:
-    def __init__(self, token: str = None) -> None:
+    def __init__(
+        self,
+        token: str = None
+        ) -> None:
         self.token = token
-        self.header = {"Authorization" : f"Bearer {self.token}"}
-        """Client class"""
+        self.header = {
+            "Authorization" : f"Bearer {self.token}"
+            }
+        """A client class that has methods to access the twitter api
+        
+        Parameters
+        ----------
+        token: :class: `str`
+            Bare token used for authorization
+        """
     
     def help(self):
         statuscodes = [
@@ -25,53 +36,105 @@ class Client:
             "504: Gateway Timeout"
             ]
         print("\n".join(statuscodes))
-        """Help command"""
+        """Help command
+        gives http status codes"""
 
-    def get_user(self, username: str) -> None:
+    def get_user(
+        self,
+    username: str
+    ) -> None:
+        """Gets a user by username
+        Parameters
+        ----------
+        username: :class: `str`
+        """
         if username.startswith("@"):
             raise Exception("Must be the username only")
         try:
-            resp = requests.get(f"https://api.twitter.com/2/users/by/username/{username}", 
-            headers=self.header)
+            resp = requests.get(
+                f"https://api.twitter.com/2/users/by/username/{username}", 
+            headers=self.header
+            )
             data = json.loads(resp.text)
-            print(resp.status_code, data)
+            print(
+                resp.status_code,
+                data
+                )
         except Exception as errorcode:
             raise errorcode
-        """Getuser command, makes a request to the twitter api for a user"""
 
-    def get_users(self, *usernames: str) -> None:
+    def get_users(
+        self,
+        *usernames: str
+        ) -> None:
+        """Gets users by usernames
+        Parameters
+        ----------
+        usernames: :class: `str`
+        """
         if "@" in usernames:
             raise Exception("Must be the username only")
         try:
             newids = ",".join(usernames)
-            resp = requests.get(f"https://api.twitter.com/2/users/by/username/{newids}", 
-            headers=self.header)
+            resp = requests.get(
+                f"https://api.twitter.com/2/users/by/username/{newids}", 
+            headers=self.header
+            )
             data = json.loads(resp.text)
-            print(resp.status_code, data)
+            print(
+                resp.status_code,
+                data
+                )
         except Exception as errorcode:
             raise errorcode
-        """Getusers command, makes a request to the twitter api for a group of users"""
 
 
-    def get_tweet(self, id: int) -> None:
-        if isisntance(id, str):
+    def get_tweet(
+        self,
+        id: int
+        ) -> None:
+        """Gets a tweet by id
+        Parameters
+        ----------
+        id: :class: `int`
+        """
+        if isinstance(
+            id,
+            str
+            ):
             raise Exception("id must be an int not a string")
         try:
-            resp = requests.get(f"https://api.twitter.com/2/tweets?ids={id}",
-            headers=self.header)
+            resp = requests.get(
+                f"https://api.twitter.com/2/tweets?ids={id}",
+                headers=self.header
+                )
             data = json.loads(resp.text)
-            print(resp.status_code, data)
+            print(
+                resp.status_code,
+                data
+                )
         except Exception as errorcode:
             raise errorcode
-        """Getuser command, makes a request to the twitter api for a tweet"""
 
-    def get_tweets(self, *ids: int) -> None:
+    def get_tweets(
+        self,
+    *ids: int
+    ) -> None:
+        """Gets tweets by ids
+        Parameters
+        ----------
+        ids: :class: `int`
+        """
         try:
             newids = ",".join(ids)
-            resp = requests.get(f"https://api.twitter.com/2/tweets?ids={newids}", 
-            headers=self.header)
+            resp = requests.get(
+                f"https://api.twitter.com/2/tweets?ids={newids}", 
+                headers=self.header
+                )
             data = json.loads(resp.text)
-            print(resp.status_code, data)
+            print(
+                resp.status_code,
+                data
+                )
         except Exception as errorcode:
             raise errorcode
-        """Getusers command, makes a request to the twitter api for a group of tweets"""
